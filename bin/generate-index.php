3d83591f9aa99236a984f166af61b7a38c3a645a
piwi <me@e-piwi.fr>
2014-08-14 22:15:59 +0200

    adding the Curtis icon + first index.html

--- bin/generate-index.php
+++ bin/generate-index.php
@@ -99,20 +99,20 @@ $metas = '{% META %}';
 
 // MDE repository
 $stamp_url = 'http://github.com/markdown-extended/manifest';
-$stamp_title = 'See the Markdown Extended specifications sources';
+$stamp_title = 'See the MDE specifications sources on Github';
 
 // page notice
-//$page_notice = 'Content rendered from a <a href="http://github.com/piwi/markdown-extended" title="github.com/piwi/markdown-extended">Markdown Extended</a> content&nbsp;&dash;&nbsp;<a href="?plain" title="See plain text version of this content">See raw content</a>';
+$page_notice = 'Content rendered from a <a href="http://github.com/piwi/markdown-extended" title="github.com/piwi/markdown-extended">Markdown Extended</a> content&nbsp;&dash;&nbsp;<a href="?plain" title="See plain text version of this content">See raw content</a>';
 
 // MathJax.js
 //$scripts = array('http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML');
 
 // options
 $settings = array();
-$settings['app_mode'] = 'dev';
-$settings['charset'] = '{% CHARSET %}';
-$settings['brand_icon'] = '<i class="fa fa-file-text"></i>';
-$settings['brand_title'] = '{% TITLE %}';
+//$settings['app_mode'] = 'dev';
+//$settings['charset'] = '{% CHARSET %}';
+$settings['brand_icon'] = '<img src="modules/markdown-mark/png/32x20-solid.png" />';
+$settings['brand_title'] = 'the MDE specifications';
 $settings['menu_item_content_stamp'] = function() use (&$stamp_url, &$stamp_title) {
     $icon = hqt_safestring(hqt_setting('menu_item_content_stamp_icon'));
     return '<a title="'.hqt_safestring($stamp_title).'" href="'.hqt_safestring($stamp_url).'">'.$icon.'MDE</a>';
@@ -124,16 +124,15 @@ $settings['language_strings']['notes_block_header'] = '';
 $settings['profiler_user_stack'] = array(
     'MDE' => $mde_version
 );
-$settings['app_name'] = $mde_version;
+$settings['app_name'] = 'the MDE manifest';
 $settings['app_description'] = 'The new way of writing for the web';
 $settings['app_infos'] = array(
-    'license' => '<a href="http://opensource.org/licenses/BSD-3-Clause" title="See online">BSD-3-Clause open source license</a>',
+    'license' => '<a href="http://creativecommons.org/licenses/by/3.0/" title="See online">Creative Commons BY 3.0</a>',
     'maintainer' => '<a href="http://github.com/piwi" title="See online">@pierowbmstr</a>',
-    'sources &amp; updates' => '<a href="http://github.com/piwi/markdown-extended" title="See online">http://github.com/piwi/markdown-extended</a>',
-    'documentation' => '<a href="http://aboutmde.org/" title="See online">aboutmde.org</a>',
+    'sources &amp; updates' => '<a href="http://github.com/markdown-extended/manifest" title="See online">http://github.com/markdown-extended/manifest</a>',
 );
 $settings['app_dependencies'] = array(
-    array('name'=>'PHP Markdown Extended', 'version'=>'gamma', 'home'=>'http://github.com/piwi/markdown-extended', 'license'=>'BSD 3.0 license', 'license_url'=>'http://opensource.org/licenses/BSD-3-Clause'),
+    array('name'=>$mde_version, 'home'=>'http://github.com/piwi/markdown-extended', 'license'=>'BSD 3.0 license', 'license_url'=>'http://opensource.org/licenses/BSD-3-Clause'),
     array('name'=>'html5 quick template', 'version'=>'1.2.1', 'home'=>'http://github.com/piwi/html5-quick-template', 'license'=>'Apache 2.0 license', 'license_url'=>'http://www.apache.org/licenses/LICENSE-2.0.html'),
     array('name'=>'jQuery', 'version'=>'1.11.0', 'home'=>'http://jquery.com/', 'license'=>'MIT license', 'license_url'=>'http://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt'),
     array('name'=>'Bootstrap', 'version'=>'3.1.1', 'home'=>'http://getbootstrap.com/', 'license'=>'Apache license v2.0', 'license_url'=>'http://www.apache.org/licenses/LICENSE-2.0'),
@@ -141,8 +140,8 @@ $settings['app_dependencies'] = array(
     array('name'=>'HTML5shiv', 'version'=>'3.7.0', 'home'=>'http://code.google.com/p/html5shiv/', 'license'=>'MIT license', 'license_url'=>'http://www.opensource.org/licenses/mit-license.php'),
     array('name'=>'Respond.js', 'version'=>'1.4.2', 'home'=>'http://github.com/scottjehl/Respond', 'license'=>'MIT license', 'license_url'=>'http://www.opensource.org/licenses/mit-license.php'),
 );
-//$settings['app_about_notice'] = 'To follow sources updates, create a fork of the template or transmit a bug, please have a look at the GitHub repository at <a href="http://github.com/markdown-extended" title="See sources on GitHub">piwi/markdown-extended</a>.';
-//$settings['app_manual_url'] = 'http://aboutmde.org';
+$settings['app_about_notice'] = 'To follow specifications updates or transmit a bug, please have a look at the GitHub repository at <a href="http://github.com/markdown-extended/manifest" title="See sources on GitHub">markdown-extended/manifest</a>.';
+$settings['app_manual_url'] = 'https://github.com/markdown-extended/manifest/tree/gh-pages';
 
 // generate HTML template
 ob_start();
@@ -153,11 +152,11 @@ ob_end_clean();
 // write it in $target_file
 $target_file        = settings('document_root').settings('target_file');
 $tmp_target_file    = settings('document_root').settings('tmp_target_file');
-$mde_manifest       = settings('mde_manifest', 'mde-manifest.md');
+$mde_manifest       = settings('document_root').settings('mde_manifest');
 if ($ok = file_put_contents($tmp_target_file, $_tpl, LOCK_EX)) {
-    info("template updated in file '$tmp_target_file' with string of length ".strlen($_tpl));
-    if ($ok = exec('php '.$mde_console.' -t '.$tmp_target_file.' -o '.$tmp_target_file.' '.$mde_manifest)) {
-        info("index updated in file '$target_file' parsing '$mde_manifest' with string of length ".strlen($_tpl));
+    echo "OK > template updated in file '$tmp_target_file' with string of length ".strlen($_tpl).PHP_EOL;
+    if ($ok = exec('php '.$mde_console.' -t='.$tmp_target_file.' -o='.$target_file.' '.$mde_manifest)) {
+        echo "OK > index updated in file '$target_file' parsing '$mde_manifest'".PHP_EOL;
     } else {
         error("an error occured while trying to write in file '$target_file'!");
     }
